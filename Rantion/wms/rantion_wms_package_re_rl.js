05ff22e675a9d7fbef38af41d46cc3a10abd6704
LICLIN10 <licanlin95@163.com>
2020-07-22 16:39:38 +0800

    update 2020-07-22T08:39:26.662Z

--- Rantion/wms/rantion_wms_package_re_rl.js
+++ Rantion/wms/rantion_wms_package_re_rl.js
@@ -2,7 +2,7 @@
  * @Author         : Li
  * @Version        : 1.0
  * @Date           : 2020-05-22 17:01:38
- * @LastEditTime   : 2020-07-22 15:45:39
+ * @LastEditTime   : 2020-07-22 16:38:49
  * @LastEditors    : Li
  * @Description    : 
  * @FilePath       : \Rantion\wms\rantion_wms_package_re_rl.js
@@ -12,11 +12,9 @@
  *@NApiVersion 2.x
  *@NScriptType Restlet
  */
-define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logistics_cost_calculation', 'N/runtime'], function (search, record, log, runtime, task, costCal, runtime) {
-
-    function _get(context) {
-
-    }
+define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logistics_cost_calculation',
+    'N/runtime'
+], function (search, record, log, runtime, task, costCal, runtime) {
 
     function _post(context) {
 
@@ -24,31 +22,8 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
         //     {
         //       "aono": "aono202003",
         //       "boxNo": "1",
-        //       "detailModels": [
-        //         {
-        //           "asin": "",
-        //           "boxNo": "1",
-        //           "fnsku": "",
-        //           "msku": "",
-        //           "productCode": "",
-        //           "productImageUrl": "",
-        //           "productTitle": "",
-        //           "productType": 10,
-        //           "qty": 1,
-        //           "sku": "SKU0520"
-        //         },
-        //         {
-        //           "asin": "",
-        //           "boxNo": "1",
-        //           "fnsku": "",
-        //           "msku": "",
-        //           "productCode": "",
-        //           "productImageUrl": "",
-        //           "productTitle": "",
-        //           "productType": 10,
-        //           "qty": 1,
-        //           "sku": "SKU666"
-        //         }
+        //       "detailModels": [ { "asin": "", "boxNo": "1", "fnsku": "", "msku": "", "productCode": "", "productImageUrl": "", "productTitle": "", "productType": 10, "qty": 1, "sku": "SKU0520" },
+        //         { "asin": "", "boxNo": "1", "fnsku": "", "msku": "", "productCode": "", "productImageUrl": "", "productTitle": "", "productType": 10, "qty": 1, "sku": "SKU666" }
         //       ],
         //       "height": 20.00,
         //       "length": 10.00,
@@ -57,50 +32,22 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
         //     }
         //   ]
 
-
-        // WMS 装柜回传
-        // {
-        //     "containerNo": "123",
-        //     "shippingType": 10,
-        //     "sourceWarehouseCode": "gzhdc",
-        //     "sourceWarehouseName": "gzhdc",
-        //     "targetWarehouseCode": "TEST",
-        //     "targetWarehouseName": "TEST",
-        //     "containerVolume": 100,
-        //     "createBy": "徐玉立"
-        // }
-
-
         var a_start_time = new Date().getTime();
         log.audit("处理数据开始时间, Starts", new Date().toISOString());
         var data = context;
 
-
-        var fulRecArr = [];
-
         log.audit('data typeof(): ' + typeof (data), data);
 
-        var retjson = {};
-
-        var item_box = [];
-        var aono, ord = [];
-
+        var retjson = {},
+            item_box = [],
+            aono, ord = [];
         var scriptObj = runtime.getCurrentScript();
 
         try {
-
             log.audit('data length', data.length);
 
-
-            // 用于标记当前发运记录是否发运完成, 默认 为 false
-            var flag = false;
-            var totalWeight = 0;
-
-            var serverID;
-            var rec_country;
-            var zip;
-            var tLocation;
-            var city;
+            var totalWeight = 0,
+                serverID, rec_country, zip, tLocation, city;
             var rid = searchTranRec(data[0].aono);
             if (rid) {
                 search.create({
@@ -139,31 +86,20 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                 });
                 log.audit("serverID rec_country city location", serverID + '-' + rec_country + '-' + city + '-' + tLocation);
 
-
                 var objRecord = record.load({
                     type: 'customrecord_dps_shipping_record',
                     id: rid
                 });
 
-
                 var sub_id = 'recmachcustrecord_dps_ship_box_fa_record_link';
-
                 var status = objRecord.getValue("custrecord_dps_shipping_rec_status");
                 var numLines = objRecord.getLineCount({
                     sublistId: sub_id
                 });
 
-
-                var boxNum = objRecord.getValue('custrecord_dps_total_number'),
-                    addNum = 1;
-
-                var add = 0;
-
-                // log.debug('objRecord', JSON.stringify(objRecord));
                 log.debug('发运记录装箱明细行数 status', numLines + '-' + status);
                 if (status != 14 && numLines > -1) {
                     var it = {
-
                         code: 1,
                         data: null,
                         msg: '非法操作'
@@ -172,14 +108,12 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                     return it;
                 }
 
-
                 for (var i = 0, len = data.length; i < len; i++) {
                     log.audit('存在明细: ' + i, detailModels);
                     var temp = data[i];
                     totalWeight += temp.weight;
                     var detailModels = temp.detailModels;
                     aono = temp.aono;
-
 
                     objRecord.setSublistValue({
                         sublistId: sub_id,
@@ -219,12 +153,8 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                         log.debug('detailModels.length', detailModels.length)
                         for (var j = 0, j_len = detailModels.length; j < j_len; j++) {
 
-                            add++;
-
-                            // var a = j + i;
                             var a = i;
                             var arrTemp = detailModels[j];
-
                             if (numLines > -1) {
                                 a += numLines;
                             }
@@ -235,18 +165,14 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                                 line: i,
                                 text: arrTemp.sku
                             });
-
                             objRecord.setSublistValue({
                                 sublistId: sub_id,
                                 fieldId: 'custrecord_dps_ship_box_quantity',
                                 line: i,
                                 value: arrTemp.qty
                             });
-                            // log.debug("Remaining governance units: " + scriptObj.getRemainingUsage());
                         }
-
                     } else {
-
                         log.audit('不存在明细')
                         var sub_id = 'recmachcustrecord_dps_ship_box_fa_record_link';
 
@@ -280,17 +206,13 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                             line: 0,
                             value: temp.height
                         });
-
                     }
-
                 }
-
 
                 objRecord.setValue({
                     fieldId: 'custrecord_dps_total_number',
                     value: data.length
                 });
-
                 var objRecord_id = objRecord.save();
                 log.audit('objRecord_id', objRecord_id);
 
@@ -307,7 +229,6 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                         'custrecord_dps_shipping_rec_estimatedfre': cost
                     }
                 });
-
                 log.audit("执行总共使用量: " + scriptObj.getRemainingUsage());
             } else {
                 log.error('找不到对应的发运记录', "找不到对应的发运记录");
@@ -316,77 +237,17 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
                 retjson.msg = 'unknown ' + data[0].aono;
             }
         } catch (error) {
-
             log.error('error', error);
             retjson.code = 5;
             retjson.data = null;
             retjson.msg = 'error';
         }
 
-        // retjson.code = 0;
-        // retjson.data = {};
-        // retjson.msg = 'string';
-
         log.audit("处理时间, End", new Date().toISOString());
-
         var a_end_time = new Date().getTime();
-        a_start_time
 
         log.audit('处理数据总共耗时： ', (a_end_time - a_start_time) / 1000 + " s");
         return JSON.stringify(retjson);
-    }
-
-
-    /**
-     * 根据 调拨单号 搜索对应的发运记录, 获取相关的货品信息
-     * @param {*} aono 
-     */
-    function searchRecord(aono) {
-
-        var ord = [];
-        search.create({
-            type: 'customrecord_dps_shipping_record',
-            filters: [{
-                name: 'custrecord_dps_shipping_rec_order_num',
-                operator: 'anyof',
-                values: aono
-            }],
-            columns: [{
-                    name: "custrecord_dps_ship_record_sku_item",
-                    join: "CUSTRECORD_DPS_SHIPPING_RECORD_PARENTREC"
-                },
-                {
-                    name: "custrecord_dps_shipping_record_item",
-                    join: "CUSTRECORD_DPS_SHIPPING_RECORD_PARENTREC"
-                },
-                {
-                    name: "custrecord_dps_ship_record_item_quantity",
-                    join: "CUSTRECORD_DPS_SHIPPING_RECORD_PARENTREC"
-                }
-            ]
-        }).run().each(function (rec) {
-            var it = {
-                recId: rec.id,
-                sku: rec.getValue({
-                    name: "custrecord_dps_ship_record_sku_item",
-                    join: "CUSTRECORD_DPS_SHIPPING_RECORD_PARENTREC"
-                }),
-                itemId: rec.getValue({
-                    name: "custrecord_dps_shipping_record_item",
-                    join: "CUSTRECORD_DPS_SHIPPING_RECORD_PARENTREC"
-                }),
-                quantity: rec.getValue({
-                    name: "custrecord_dps_ship_record_item_quantity",
-                    join: "CUSTRECORD_DPS_SHIPPING_RECORD_PARENTREC"
-                })
-            };
-
-            ord.push(it);
-
-            return true;
-        });
-
-        return ord;
     }
 
 
@@ -414,115 +275,7 @@ define(['N/search', 'N/record', 'N/log', 'N/runtime', 'N/task', '../Helper/logis
     }
 
 
-    /**
-     * 搜索货品
-     * @param {*} itemId 
-     */
-    function searchItem(itemId) {
-
-        var item;
-        search.create({
-            type: 'item',
-            filters: [{
-                name: 'itemid',
-                operator: 'is',
-                values: itemId
-            }]
-        }).run().each(function (rec) {
-            item = rec.id;
-        });
-
-        return item || false;
-    }
-
-
-    function Deduplication(arr) {
-        var newArr = [],
-            newData = [];
-        var len = arr.length;
-        for (var i = 0; i < len; i++) {
-            var temp = arr[i];
-
-            var aono_i = temp.aono;
-            for (var j = i + 1; j < len; j++) {
-                var aono_j = arr[j].aono;
-                if (aono_i == aono_j) {
-                    newData.push(arr[j]);
-                }
-            }
-
-            newData.push(arr[i]);
-            var it = {
-                aono: arr[i].aono,
-                data: newData
-            }
-
-            newArr.push(it);
-
-        }
-
-        return newArr || false;
-
-    }
-
-    function _put(context) {
-
-    }
-
-    function _delete(context) {
-
-    }
-
-
     return {
-        get: _get,
         post: _post,
-        put: _put,
-        delete: _delete
     }
 });
-
-
-/**
- * aono  作为KEY，其他作为数组返回一个obj
- * @param {*} data 
- */
-function getDataObj(data) {
-    var data_obj = {},
-        data_arrays = [],
-        newArr = [],
-        f = true
-    data.map(function (ob) {
-        f = true
-        for (var key in data_obj) {
-            if (key == ob.aono) {
-                f = false
-                delete ob.aono;
-                data_obj[key].push(ob)
-            }
-        }
-        if (f) {
-            data_arrays = []
-            var ao = ob.aono
-            delete ob.aono;
-            data_arrays.push(ob)
-            data_obj[ao] = data_arrays
-        }
-    });
-
-    var keys1 = [];
-    for (var p1 in data_obj) {
-        if (data_obj.hasOwnProperty(p1))
-            keys1.push(p1);
-    }
-
-    for (var ki = 0, len = keys1.length; ki < len; ki++) {
-        var it = {
-            key: keys1[ki],
-            data: data_obj[keys1[ki]]
-        };
-        newArr.push(it);
-    }
-    return newArr;
-    return data_obj
-}
